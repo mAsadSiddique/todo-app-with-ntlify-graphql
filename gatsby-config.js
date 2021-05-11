601472b32b69ec63f15c3c6aa20309340fe8fadd
mAsadSiddique <asadsiddique.a@gmail.com>
2021-05-12 01:28:53 +0500

    Add authentication

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,12 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-theme-ui`,
+      options: {
+        preset: `gatsby-theme-ui-preset`,
+      },
+    },
+  ],
 }
